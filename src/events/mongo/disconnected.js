d8086bfe62eefdee5c6a62943545d01ef103e37d
Yang Husurianto <yang.husurianto@gmail.com>
2024-01-26 21:55:41 -0700

    fix: temp disable reconnect on disconnect

--- src/events/mongo/disconnected.js
+++ src/events/mongo/disconnected.js
@@ -6,9 +6,9 @@ module.exports = {
   async execute(client) {
     console.log(chalk.red("[Database Status]: Disonnected!!! :c"))
 
-    console.log(chalk.cyan("[Database Status]: Reconnecting..."))
-    mongoose.connect(process.env.MONGO_CONNECTION, {
-      server: { auto_reconnect: true },
-    }).catch(console.error);
+    // console.log(chalk.cyan("[Database Status]: Reconnecting..."))
+    // mongoose.connect(process.env.MONGO_CONNECTION, {
+    //   server: { auto_reconnect: true },
+    // }).catch(console.error);
   },
 };
