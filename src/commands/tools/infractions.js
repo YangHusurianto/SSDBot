9c9766bdbe9daec89e1de297395c7c4949100b3c
Yang Husurianto <yang.husurianto@gmail.com>
2024-01-27 14:46:04 -0700

    fix: infraction on nonexistent user

--- src/commands/tools/infractions.js
+++ src/commands/tools/infractions.js
@@ -50,10 +50,24 @@ module.exports = {
     var warnPage = options.getInteger('page') ?? 1;
 
     try {
-      const targetDoc = await Guild.findOne(
+      const targetDoc = await Guild.findOneAndUpdate(
         { guildId: guild.id, 'users.userId': target.id },
-        { 'users.$': 1 }
+        { 'users.$': 1 },
+        {
+          $setOnInsert: {
+            _id: new mongoose.Types.ObjectId(),
+            guildId: guild.id,
+            guildName: guild.name,
+            guildIcon: guild.iconURL(),
+            caseNumber: 0,
+            loggingChannel: '',
+            users: [],
+            autoTags: new Map(),
+          },
+        },
+        { upsert: true, new: true }
       );
+
       const warnings = targetDoc.users[0].warns;
       const maxWarnPages = Math.ceil(warnings.length / INFRACTIONS_PER_PAGE);
 
