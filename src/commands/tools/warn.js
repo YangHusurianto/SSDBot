ccdcb0a7cf7f44953da1e6938bf56ec67056677e
Yang Husurianto <yang.husurianto@gmail.com>
2024-02-09 02:22:23 -0700

    fix: refactor name miss

--- src/commands/tools/warn.js
+++ src/commands/tools/warn.js
@@ -96,7 +96,7 @@ module.exports = {
         };
 
         guildDoc.users.push(userDoc);
-      } else userDoc.warns.push(warning);
+      } else userDoc.infractions.push(warning);
 
       await guildDoc.save().catch(console.error);
 
