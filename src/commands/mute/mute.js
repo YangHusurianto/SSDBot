747081e71ed65e6c7ec316b6e8e76651ac5886a5
Yang Husurianto <yang.husurianto@gmail.com>
2024-03-31 00:10:24 -0700

    feat: add mute command and time util

---
+++ src/commands/mute/mute.js
@@ -0,0 +1,143 @@
+const { findGuild, getReplacedReason } = require('../../queries/guildQueries');
+const { findAndCreateUser } = require('../../queries/userQueries');
+const { logMessage } = require('../../utils/logMessage');
+const { botSelfCheck, roleHeirarchyCheck } = require('../../utils/checks');
+
+const { SlashCommandBuilder, escapeMarkdown } = require('discord.js');
+const mongoose = require('mongoose');
+
+const ms = require('ms');
+import prettyMilliseconds from 'pretty-ms';
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('mute')
+    .setDescription('Mute a user for a specified amount of time and reason') 
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('The user to mute')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('time')
+        .setDescription('The amount of time to mute the user for')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('reason')
+        .setDescription('The reason')
+        .setRequired(true)
+        .setAutocomplete(true)
+    )
+    .setDMPermission(false),
+
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    const guild = await findGuild(interaction.guild);
+    let tags = guild.autoTags;
+
+    const filtered = Array.from(tags).filter(([key, _value]) =>
+      key.startsWith(focusedValue)
+    );
+
+    if (!filtered.length && focusedValue.length === 0) {
+      return await interaction.respond(
+        Array.from(tags).map(([key, _value]) => ({ name: key, value: key }))
+      );
+    }
+
+    return await interaction.respond(
+      filtered.map(([key, _value]) => ({ name: key, value: key }))
+    );
+  },
+
+  async execute(interaction, client) {
+    const { options, guild, member } = interaction;
+    const target = options.getUser('user');
+    var time = options.getString('time');
+    var reason = options.getString('reason');
+
+    if (await botSelfCheck(interaction, target, client, 'warn')) return;
+    if (await roleHeirarchyCheck(interaction, guild, target, member, 'warn')) return;
+    time = ms(time);
+    if (!time) {
+      return await interaction.reply({
+        content: 'Invalid time format, please try again.',
+        ephemeral: true,
+      });
+    }
+
+    try {
+      muteUser(interaction, client, guild, target, member, time, reason);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
+
+const muteUser = async (interaction, client, guild, target, member, time, reason) => {
+  const guildDoc = await findGuild(guild);
+
+  reason = await getReplacedReason(guild, reason);
+
+  // create the warning first so we can insert regardless of whether the user exists
+  const mute = {
+    _id: new mongoose.Types.ObjectId(),
+    guildId: guild.id,
+    targetUserId: target.id,
+    type: 'MUTE',
+    number: guildDoc.caseNumber,
+    reason: reason,
+    date: new Date(),
+    time: time,
+    moderatorUserId: member.user.id,
+    moderatorNotes: '',
+  };
+
+  let userDoc = await findAndCreateUser(guild.id, target.id, true);
+  userDoc.infractions.push(mute);
+
+  guildDoc.caseNumber++;
+  await guildDoc.save().catch(async (err) => {
+    await interaction.reply(`:x: Failed to update case number.`);
+    console.error(err);
+  });
+
+  await userDoc.save().catch(async (err) => {
+    await interaction.reply(`:x: Failed to save mute.`);
+    console.error(err);
+  });
+
+  const targetMember = await guild.members.fetch(target.id);
+  const savedRoles = targetMember.roles.valueOf();
+
+  targetMember.roles.set(['878334561094873109']);
+
+  const formattedTime = prettyMilliseconds(time, { verbose: true });
+  await interaction.reply(
+    `<:check:1196693134067896370> ${target} has been muted for ${formattedTime}.`
+  );
+
+  //log to channel
+  await logMessage(
+    guild,
+    `**MUTE** | Case #${guildDoc.caseNumber - 1}\n` +
+      `**Target:** ${escapeMarkdown(`${target.username} (${target.id}`, {
+        code: true,
+      })})\n` +
+      `**Moderator:** ${escapeMarkdown(
+        `${member.user.username} (${member.user.id}`,
+        { code: true }
+      )})\n` +
+      `**Time:** ${formattedTime}\n` +
+      `**Reason:** ${reason}\n`
+  );
+
+  setTimeout(() => {
+    target.roles.set(savedRoles);
+  }, time) 
+};
