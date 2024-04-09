b0d913d0422fc78d0e2a47ff308796c6681b50e6
Yang H <yang.husurianto@gmail.com>
2024-04-08 20:40:28 -0700

    feat: delete message command

--- src/utils/checks.js
+++ src/utils/checks.js
@@ -1,21 +1,19 @@
 export async function botSelfCheck(interaction, target, client, type) {
-  let check = false;
-
   if (target.id === client.user.id) {
-    check = await interaction.reply({
+    return await interaction.reply({
       content: `I cannot ${type} myself!`,
       ephemeral: true,
     });
   }
 
   if (target.id === interaction.member.id) {
-    check = await interaction.reply({
+    return await interaction.reply({
       content: `You cannot ${type} yourself!`,
       ephemeral: true,
     });
   }
 
-  return check;
+  return false;
 }
 
 export async function roleHeirarchyCheck(
@@ -25,27 +23,25 @@ export async function roleHeirarchyCheck(
   member,
   type
 ) {
-  let check = false;
-
-  await guild.members
+  return await guild.members
     .fetch(target.id)
     .then(async (targetMember) => {
       if (
         member.roles.highest.comparePositionTo(targetMember.roles.highest) < 1
       ) {
-        check = await interaction.reply({
+        return await interaction.reply({
           content: `You cannot ${type} a member with a higher or equal role than you!`,
           ephemeral: true,
         });
       }
+
+      return false;
     })
     .catch(async (err) => {
       console.error(err);
-      check = await interaction.reply({
+      return await interaction.reply({
         content: 'Failed to fetch member for permissions check.',
         ephemeral: true,
       });
     });
-
-  return check;
 }
