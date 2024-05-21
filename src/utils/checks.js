03eae54bb8ee772ae3fd91858a23422bfc2817be
Yang H <yang.husurianto@gmail.com>
2024-05-20 23:27:25 -0700

    add deferred reply checking

--- src/utils/checks.js
+++ src/utils/checks.js
@@ -1,16 +1,20 @@
 export async function botSelfCheck(interaction, target, client, type) {
   if (target.id === client.user.id) {
-    return await interaction.reply({
+    const selfCheck = {
       content: `I cannot ${type} myself!`,
       ephemeral: true,
-    });
+    };
+    if (interaction.replied) return await interaction.editReply(selfCheck);
+    else return await interaction.reply(selfCheck);
   }
 
   if (target.id === interaction.member.id) {
-    return await interaction.reply({
+    const selfCheck = {
       content: `You cannot ${type} yourself!`,
       ephemeral: true,
-    });
+    };
+    if (interaction.replied) return await interaction.editReply(selfCheck);
+    else return await interaction.reply(selfCheck);
   }
 
   return false;
@@ -29,29 +33,35 @@ export async function roleHeirarchyCheck(
       if (
         member.roles.highest.comparePositionTo(targetMember.roles.highest) < 1
       ) {
-        return await interaction.reply({
+        const banCheck = {
           content: `You cannot ${type} a member with a higher or equal role than you!`,
           ephemeral: true,
-        });
+        };
+        if (interaction.replied) return await interaction.editReply(banCheck);
+        else return await interaction.reply(banCheck);
       }
 
       return false;
     })
     .catch(async (err) => {
-      
       if (type === 'ban') {
-        await interaction.reply({
-          content: 'Failed to fetch member for permissions check. Attempting to ban user anyway...',
+        const banCheck = {
+          content:
+            'Failed to fetch member for permissions check. Attempting to ban user anyway...',
           ephemeral: true,
-        });
+        };
+        if (interaction.replied) await interaction.editReply(banCheck);
+        else await interaction.reply(banCheck);
 
         return false;
       }
 
       console.error(err);
-      return await interaction.reply({
+      const banCheck = {
         content: 'Failed to fetch member for permissions check.',
         ephemeral: true,
-      });
+      };
+      if (interaction.replied) return await interaction.editReply(banCheck);
+      else return await interaction.reply(banCheck);
     });
 }
