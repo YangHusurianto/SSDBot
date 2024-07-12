e3e4560c8b4b6f99252667e921620f537cfe7c7f
Yang H <yang.husurianto@gmail.com>
2024-07-11 20:13:02 -0700

    remove file

--- src/functions/handlers/handleCache.ts
+++
@@ -1,9 +0,0 @@
-import { findGuild } from '../../queries/guildQueries.js';
-
-import { Client } from 'discord.js';
-
-export async function loadCache(client: Client) {
-  const guilds = client.guilds.cache.map((guild) => guild.id);
-
-  console.log(guilds)
-}
